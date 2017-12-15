2e41735cf9b4136bcb7fd8e250dee6bb1683f325
sonukr <sonuyadav9756@gmail.com>
2017-12-16 00:38:55 +0530

    read me updated

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
-echo “Hey” 
+echo “Hey There...!!”
+
+echo "Starting  your project"
+
+echo "opening your project"
+atom .
+
+echo "Starting the Gulp task"
+gulp
+
+# open -a iTerm .
+# open -a iTerm . command "gulp"
+
+echo "Starting the react server"
+npm start
