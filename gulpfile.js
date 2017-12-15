6537659ed3a864e4ae1934a9c9453128c358573a
Sonu <sonu@Sonus-MacBook-Pro.local>
2017-12-15 22:48:03 +0530

    gulp sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del = require('del');
 
-
-gulp.task('default', function() {
-  // place code for your default task here
+// create a task for compiling scss to css
+gulp.task('sass', ['copy'], function () {
+   gulp.src('./src/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css/'));
 });
+
+// create a task to watch the scss changes
+gulp.task('watch', function () {
+  gulp.watch('./src/scss/*.scss', ['sass', 'clean']);
+});
+
+
+
+//call the default task in which call above task as dependency
+gulp.task('default', ['sass', 'watch']);
